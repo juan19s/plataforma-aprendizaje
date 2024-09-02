bc635f5994a728280d9f8921ac9a2b82bf972dbb
Juan Pedraza <pedrazajuanmec@gmail.com>
2024-09-02 09:43:04 -0600

    card avanzada con nuevo for angular 17

---
+++ src/app/views/components/card-course-learning/card-course-learning.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-card-course-learning',
+  standalone: true,
+  imports: [CardModule, ButtonModule],
+  templateUrl: './card-course-learning.component.html',
+  styleUrl: './card-course-learning.component.css',
+})
+export class CardCourseLearningComponent {
+  cards: { id: number; title: string; description: string; image: string }[] = [
+    {
+      id: 1,
+      title: 'Learn Angular',
+      description: 'Learn Angular with this course',
+      image: 'assets/images/card_prog.png',
+    },
+    {
+      id: 2,
+      title: 'Master React',
+      description: 'Become proficient in React with our comprehensive course',
+      image: 'assets/images/card_prog.png',
+    },
+    {
+      id: 3,
+      title: 'Explore Vue.js',
+      description: 'Dive into Vue.js and build amazing applications',
+      image: 'assets/images/card_prog.png',
+    },
+  ];
+}
--- src/app/views/components/main-content/main-content.component.ts
+++ src/app/views/components/main-content/main-content.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CardCourseLearningComponent } from '../card-course-learning/card-course-learning.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [],
+  imports: [CardCourseLearningComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.css'
 })
