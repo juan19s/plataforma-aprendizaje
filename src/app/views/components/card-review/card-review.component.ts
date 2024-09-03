b07ae38416d40a2ec80fbfab66d0e623e7ed48ec
Juan Pedraza <pedrazajuanmec@gmail.com>
2024-09-03 12:52:12 -0600

    Seccion de revies, 1era parte

---
+++ src/app/views/components/card-review/card-review.component.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { RatingModule } from 'primeng/rating';
+import { BadgeModule } from 'primeng/badge';
+
+@Component({
+  selector: 'app-card-review',
+  standalone: true,
+  imports: [ButtonModule, RatingModule, FormsModule, BadgeModule],
+  templateUrl: './card-review.component.html',
+  styleUrl: './card-review.component.css',
+})
+export class CardReviewComponent {
+  reviews: {
+    id: number;
+    person: string;
+    position: string;
+    image: string;
+    starring: number;
+    categorie: string;
+  }[] = [
+    {
+      id: 1,
+      person: 'Angela Aguilar',
+      position: 'Senior Software Engineer',
+      image: 'assets/images/angela.jfif',
+      starring: 5,
+      categorie: 'Diseño web',
+    },
+    {
+      id: 2,
+      person: 'Humberto Taboada',
+      position: 'Freelance Software Engineer',
+      image: 'assets/images/humberto.jfif',
+      starring: 4,
+      categorie: 'Redes',
+    },
+    {
+      id: 3,
+      person: 'Hitler Rios',
+      position: '💀',
+      image: 'assets/images/hitler.jpg',
+      starring: 5,
+      categorie: 'Fron-end',
+    },
+    {
+      id: 4,
+      person: 'Natanael Cano',
+      position: 'El rey de los corridos tumbados 👑',
+      image: 'assets/images/natanael.jfif',
+      starring: 3,
+      categorie: 'Corridos tumbados',
+    },
+  ];
+}
--- src/app/views/components/main-content/main-content.component.ts
+++ src/app/views/components/main-content/main-content.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { CardCourseLearningComponent } from '../card-course-learning/card-course-learning.component';
 import { CategoriesCardComponent } from '../categories-card/categories-card.component';
 import { ButtonModule } from 'primeng/button';
+import { CardReviewComponent } from '../card-review/card-review.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [CardCourseLearningComponent, CategoriesCardComponent, ButtonModule],
+  imports: [
+    CardCourseLearningComponent,
+    CategoriesCardComponent,
+    ButtonModule,
+    CardReviewComponent,
+  ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.css',
 })
